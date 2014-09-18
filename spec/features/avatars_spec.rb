c4f9855389f11f71d0faafddd2e7decf3acea4b8
Dylan Richards <drichards2013@gmail.com>
2014-09-17 22:04:51 -0400

    add spec for deleting photos

--- spec/features/avatars_spec.rb
+++ spec/features/avatars_spec.rb
@@ -12,4 +12,20 @@ feature 'avatars' do
       click_on 'Create Avatar'
     }.to change(Avatar, :count).by 1
   end
+
+  scenario 'deleting an avatar' do
+    visit root_path
+    click_on 'new avatar'
+
+    attach_file 'Photo', '/Users/DylanRichards/Pictures/iPhoto Library.photolibrary/Masters/2014/08/29/20140829-061646/IMG_6679.JPG'
+    fill_in 'Name', with: 'dylan'
+    fill_in 'Description', with: 'richards'
+    click_on 'Create Avatar'
+
+    click_link 'dylan'
+    expect {
+      click_link 'delete'
+    }.to change(Avatar, :count).by -1
+
+  end
 end
