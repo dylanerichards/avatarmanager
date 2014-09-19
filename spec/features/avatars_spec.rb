a91e9ae8aca6a93ccc9722bd5028175cff076513
Dylan Richards <drichards2013@gmail.com>
2014-09-18 20:36:26 -0400

    remove unused files and directories -- clean up whitespace and

--- spec/features/avatars_spec.rb
+++ spec/features/avatars_spec.rb
@@ -4,7 +4,7 @@ feature 'avatars' do
   scenario 'creating an avatar' do
     visit root_path
     click_on 'new avatar'
-    
+
     expect {
       attach_file 'Photo', '/Users/DylanRichards/Pictures/iPhoto Library.photolibrary/Masters/2014/08/29/20140829-061646/IMG_6679.JPG'
       fill_in 'Name', with: 'Dylan'
